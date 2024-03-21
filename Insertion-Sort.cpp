f7112cc7755ec0b2235cc869b383728fa61f3465
Deep kumar <141671549+DeepkumarSingh@users.noreply.github.com>
2024-03-21 22:15:11 +0530

    Update Insertion-Sort.cpp

--- Insertion-Sort.cpp
+++ Insertion-Sort.cpp
@@ -38,6 +38,40 @@ int main()
     
     return 0;
 }
-OUTPUT:
-The Insertion Sort of the given array is : 1 2 4 7 8 10 11 
+//OUTPUT:
+//The Insertion Sort of the given array is : 1 2 4 7 8 10 11 
 
+                                                                           // By using vector(simple way to understand insertion sort 
+#include<bits/stdc++.h>
+using namespace std;
+void print(vector<int> &v)
+{
+  for(int i=0;i<v.size();i++)
+  {
+    cout<<v[i]<<" ";
+  }
+  cout<<endl;
+}
+void insertionSort(vector<int> &v)
+{
+  int n=v.size();
+  // i=0 leave just thinked that for i=0 element is already sorted 
+  for(int i=1;i<n;i++)
+  {
+    int j=i-1;
+    int key=v[i];
+    while(j>=0 && v[j]>key)
+    {
+      v[j+1]=v[j]; // main insertion sort line 
+      j--;
+    }
+    v[j+1]=key; // yaha par 0 index pe key ko store kardega 
+  }
+  
+}
+int main() {
+  vector<int> v={55,44,33,22,11};
+  insertionSort(v);
+  print(v);
+  return 0;
+}
